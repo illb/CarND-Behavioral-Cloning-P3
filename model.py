f4a7cb6ffcfb3413c66042d03dd0741d0d9be24b
kenny.lim <kenny.lim@daumkakao.com>
2017-03-20 22:38:56 +0900

    basic model

---
+++ model.py
@@ -0,0 +1,63 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+
+def read_image(source_path):
+    filename = source_path.split('/')[-1]
+    current_path = './data/IMG/' + filename
+    return cv2.imread(current_path)
+
+for line in lines:
+    center_image = read_image(line[0])
+    left_image = read_image(line[1])
+    right_image = read_image(line[2])
+    images.append(center_image)
+    images.append(left_image)
+    images.append(right_image)
+    measurement = float(line[3])
+    correction = 0.2
+    measurements.append(measurement)
+    measurements.append(measurement + correction)
+    measurements.append(measurement - correction)
+
+
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurements * -1.0)
+
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6, 5, 5, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+
+model.save('model.h5')
