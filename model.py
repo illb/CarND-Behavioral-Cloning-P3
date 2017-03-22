c7e4e7d24282732c18075a185647c47e0c7d2f04
kenny.lim <kenny.lim@daumkakao.com>
2017-03-22 21:50:56 +0900

    add dropout / l2 regularizer

--- model.py
+++ model.py
@@ -1,24 +1,36 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
+from keras.layers.advanced_activations import ELU
 from keras.callbacks import TensorBoard, ModelCheckpoint, CSVLogger
-from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+from keras.optimizers import Adam
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3), name='normalization'))
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), name='crop'))
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu', name='conv_01'))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu', name='conv_02'))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu', name='conv_03'))
-model.add(Convolution2D(64, 3, 3, activation='relu', name='conv_04'))
-model.add(Convolution2D(64, 3, 3, activation='relu', name='conv_05'))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=l2(0.001), name='conv_01'))
+#model.add(ELU())
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=l2(0.001), name='conv_02'))
+#model.add(ELU())
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=l2(0.001), name='conv_03'))
+#model.add(ELU())
+model.add(Convolution2D(64, 3, 3, activation='relu', W_regularizer=l2(0.001), name='conv_04'))
+#model.add(ELU())
+model.add(Convolution2D(64, 3, 3, activation='relu', W_regularizer=l2(0.001), name='conv_05'))
 model.add(Flatten())
-model.add(Dense(100, name='fc_01'))
-model.add(Dense(50, name='fc_02'))
-model.add(Dense(10, name='fc_03'))
+#model.add(Dropout(0.7))
+#model.add(ELU())
+model.add(Dense(100, W_regularizer=l2(0.001), name='fc_01'))
+model.add(Dropout(0.7))
+#model.add(ELU())
+model.add(Dense(50, W_regularizer=l2(0.001), name='fc_02'))
+model.add(Dropout(0.7))
+#model.add(ELU())
+model.add(Dense(10, W_regularizer=l2(0.001), name='fc_03'))
 model.add(Dense(1, name='fc_04'))
 
-name = "01_nvidia_multi_camera_and_flip"
+name = "03_nvidia_l2_regularizer"
 
 callback_tb = TensorBoard(log_dir='./logs', write_images=True)
 callback_cp = ModelCheckpoint(filepath='./checkpoint/train_' + name + '-{epoch:02d}-{val_loss:.2f}.hdf5', verbose=1,
@@ -29,9 +41,8 @@ callbacks = [callback_tb, callback_cp, callback_logger]
 import data
 train_generator, train_batch_len, validation_generator, validation_batch_len, test_generator, test_batch_len = data.generators()
 
-# model.load_weights("./checkpoint/drive-01-01.hdf5")
-
-model.compile(loss='mse', optimizer='adam')
+optimizer = Adam()
+model.compile(loss='mse', optimizer=optimizer)
 history_object = model.fit_generator(train_generator, samples_per_epoch=train_batch_len,
                                      validation_data=validation_generator, nb_val_samples=validation_batch_len,
                                      verbose=1, nb_epoch=5, callbacks=callbacks)
