9037a6fcf487cf6adc85dae7f7fb818a1d537b66
kenny.lim <kenny.lim@daumkakao.com>
2017-03-24 09:25:17 +0900

    more dropout

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
-from keras.layers.advanced_activations import ELU
 from keras.callbacks import TensorBoard, ModelCheckpoint, CSVLogger
 from keras.regularizers import l2
 from keras.optimizers import Adam
@@ -10,27 +9,20 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3), name='normalization'))
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), name='crop'))
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=l2(0.001), name='conv_01'))
-#model.add(ELU())
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=l2(0.001), name='conv_02'))
-#model.add(ELU())
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu', W_regularizer=l2(0.001), name='conv_03'))
-#model.add(ELU())
 model.add(Convolution2D(64, 3, 3, activation='relu', W_regularizer=l2(0.001), name='conv_04'))
-#model.add(ELU())
 model.add(Convolution2D(64, 3, 3, activation='relu', W_regularizer=l2(0.001), name='conv_05'))
 model.add(Flatten())
-#model.add(Dropout(0.7))
-#model.add(ELU())
+model.add(Dropout(0.5))
 model.add(Dense(100, W_regularizer=l2(0.001), name='fc_01'))
 model.add(Dropout(0.7))
-#model.add(ELU())
 model.add(Dense(50, W_regularizer=l2(0.001), name='fc_02'))
 model.add(Dropout(0.7))
-#model.add(ELU())
 model.add(Dense(10, W_regularizer=l2(0.001), name='fc_03'))
 model.add(Dense(1, name='fc_04'))
 
-name = "03_nvidia_l2_regularizer"
+name = "04_nvidia_dropout2"
 
 callback_tb = TensorBoard(log_dir='./logs', write_images=True)
 callback_cp = ModelCheckpoint(filepath='./checkpoint/train_' + name + '-{epoch:02d}-{val_loss:.2f}.hdf5', verbose=1,
